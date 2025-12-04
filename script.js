54d0056c3668bc2db2f291bea6d44dfafd656c7c
Yulia Pustovalova <stud0000293261@study.utmn.ru>
2025-12-04 20:40:46 +0500

    Практика 9 готова

--- script.js
+++ script.js
@@ -14,111 +14,148 @@ function init() {
                 hasChildren: true,
                 items: [
                     {
-                        name: 'Ulgran1',
+                        name: 'Ulgran',
                         hasChildren: true,
                         items: [
                             {
-                                name: 'SMT1',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             },
                             {
-                                name: 'SMT2',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             }
                         ]
                     },
                     {
-                        name: 'Ulgran2',
+                        name: 'Vigro Mramor',
+                        hasChildren: false,
+                        items: []
+                    },
+                    {
+                        name: 'Handmade',
                         hasChildren: true,
                         items: [
                             {
-                                name: 'SMT3',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             },
                             {
-                                name: 'SMT4',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             }
                         ]
+                    },
+                    {
+                        name: 'Vigro Glass',
+                        hasChildren: false,
+                        items: []
                     }
                 ]
-            },{
+            },
+            {
                 name: 'Фильтры',
                 hasChildren: true,
                 items: [
                     {
-                        name: 'Ulgran3',
+                        name: 'Ulgran',
                         hasChildren: true,
                         items: [
                             {
-                                name: 'SMT5',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             },
                             {
-                                name: 'SMT6',
+                                name: 'Smth',
                                 hasChildren: false,
                                 items: []
                             }
                         ]
+                    },
+                    {
+                        name: 'Vigro Mramor',
+                        hasChildren: false,
+                        items: []
                     }
                 ]
             }
         ]
     }
-
-
+    
     const items = new ListItems(document.getElementById('list-items'), data)
 
-
-  /*  items.render()*/
+    items.render()
     items.init()
-
-    /*console.log(items.renderTest(data));*/
-
+    
     function ListItems(el, data) {
         this.el = el;
         this.data = data;
 
         this.init = function () {
-            const parents = this.el.querySelectorAll('[data-parent]')
-
-            parents.forEach(parent => {
-                const open = parent.querySelector('[data-open]')
-
-                open.addEventListener('click', () => this.toggleItems(parent) )
-            })
+            this.el.addEventListener('click', (e) => {
+                const arrow = e.target.closest('[data-open]');
+                if (arrow) {
+                    const parent = arrow.closest('[data-parent]');
+                    if (parent) {
+                        this.toggleItems(parent);
+                    }
+                }
+            });
         }
 
         this.render = function () {
+            this.el.innerHTML = '';
             this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
         }
 
         this.renderParent = function (data) {
-            //проверка всех элементов на hasChildren
-            //если hasChildren, то запускаем renderParent
-            //если !hasChildren, то запускаем renderChildren
-            //возвращает рендер родительского элемента
+            if (data.hasChildren && data.items.length > 0) {
+                let childrenHTML = '';
+                data.items.forEach(item => {
+                    if (item.hasChildren && item.items.length > 0) {
+                        childrenHTML += this.renderParent(item);
+                    } else {
+                        childrenHTML += this.renderChildren(item);
+                    }
+                });
 
+return `
+                    <div class="list-item list-item_open" data-parent>
+                        <div class="list-item__inner">
+                            <img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" data-open>
+                            <img class="list-item__folder" src="img/folder.png" alt="folder">
+                            <span>${data.name}</span>
+                        </div>
+                        <div class="list-item__items">
+                            ${childrenHTML}
+                        </div>
+                    </div>
+                `;
+            } else {
+                return this.renderChildren(data);
+            }
         }
 
         this.renderChildren = function (data) {
-            //вовзращает рендер элемента без вложенности
+            return `
+                <div class="list-item" data-parent>
+                    <div class="list-item__inner">
+                        <img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" style="visibility: hidden;">
+                        <img class="list-item__folder" src="img/folder.png" alt="folder">
+                        <span>${data.name}</span>
+                    </div>
+                    <div class="list-item__items"></div>
+                </div>
+            `;
         }
 
         this.toggleItems = function (parent) {
             parent.classList.toggle('list-item_open')
         }
-
-/*        this.renderTest = function (data) {
-            return `
-            <div class="test">${data.name}</div>
-            `
-        }*/
     }
-
 }
